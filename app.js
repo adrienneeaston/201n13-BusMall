5f345dd47045a49f54d986c787ffd524d6a5f87f
Adrienne <adrienneeaston@gmail.com>
2019-03-03 18:50:02 -0800

    Plan written out in comments

---
+++ app.js
@@ -0,0 +1,17 @@
+// Create an constructor with at least the properties of 
+// image name, filepath, number of times it has been shown, and the number of times it has been clicked.
+
+// Create objects for each item
+
+// items shoud be able to be randomly generated and displayed into HTML
+// Create and use a counter to keep track of how many times each item is displayed.
+
+// Create a listener that will listen for which item was clicked
+// Create and use a counter to keep track of how many times an item is chosen.
+
+// Display new items and repeat the last two steps. Do this 25 times using a for loop.
+// Don't repeat items in the display and for the next round.
+
+// For each item, calculate the percentage of times it was chosen using the two counters.
+
+// Put results into a string for each item. Append these strings to HTML.
